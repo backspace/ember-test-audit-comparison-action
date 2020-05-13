0ed0e111cabaad87800e9e954f9b898fd9fb3cc7
Buck Doyle <buck@hashicorp.com>
2020-05-13 13:46:43 -0500

    Add humanisation of durations

--- test/generate-report.js
+++ test/generate-report.js
@@ -2,31 +2,31 @@ import { expect } from 'chai';
 const generateReport = require('../lib/generate-report');
 
 describe('Generate report', function () {
-  it('should calculate changes with explicit directional indicators', function () {
+  it('should calculate changes with explicit directional indicators and humanised durations', function () {
     const generated = generateReport({
       baseReport: {
         passes: 4,
         failures: 0,
         flaky: 0,
-        duration: 1000,
+        duration: 1200,
       },
       diffReport: {
         passes: 2,
         failures: 1,
         flaky: 1,
-        duration: 2000,
+        duration: 2500,
       },
       baseIdentifier: 'base',
       diffIdentifier: 'diff',
     });
 
     const expected = `
-|               | ${'base'}      | ${'diff'}      | change                                       |
-|---------------|------------------------|------------------------|----------------------------------------------|
-| passes        | ${4}   | ${2}   | ${-2}     |
-| failures      | ${0} | ${1} | ${'+1'} |
-| flaky         | ${0}    | ${1}    | ${'+1'}       |
-| duration (ms) | ${1000} | ${2000} | ${'+1000'} |
+|          | ${'base'}      | ${'diff'}      | change                                       |
+|----------|------------------------|------------------------|----------------------------------------------|
+| passes   | ${4}   | ${2}   | ${-2}     |
+| failures | ${0} | ${1} | ${'+1'} |
+| flaky    | ${0}    | ${1}    | ${'+1'}       |
+| duration | ${'01s 200ms'} | ${'02s 500ms'} | ${'+01s 300ms'} |
 `;
     
     expect(generated).to.equal(expected);
