205ade978da35b30a7137e78b15f4aea8daf2076
Buck Doyle <buck@hashicorp.com>
2020-05-13 13:18:15 -0500

    Extract report-generation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const { getInput } = require('@actions/core');
+const generateReport = require('lib/generate-report');
 
 const baseFilePath = getInput('base-report-path', { required: true });
 const diffFilePath = getInput('comparison-report-path', { required: true });
@@ -12,22 +13,11 @@ const diffIdentifier = getInput('comparison-identifier', { required: false });
 const baseReport = JSON.parse(fs.readFileSync(baseFilePath));
 const diffReport = JSON.parse(fs.readFileSync(diffFilePath));
 
-const baseCount = baseReport.passes + baseReport.failures + baseReport.flaky;
-const diffCount = diffReport.passes + diffReport.failures + diffReport.flaky;
-
-const countDiff = diffCount - baseCount;
-const timeDiff = diffReport.duration - baseReport.duration;
-
-console.log(`Change in number of tests run: ${countDiff}`);
-console.log(`Change in total milliseconds for tests: ${timeDiff}`);
-
-const output = `
-|               | ${baseIdentifier}      | ${diffIdentifier}      | change                                       |
-|---------------|------------------------|------------------------|----------------------------------------------|
-| passes        | ${baseReport.passes}   | ${diffReport.passes}   | ${diffReport.passes - baseReport.passes}     |
-| failures      | ${baseReport.failures} | ${diffReport.failures} | ${diffReport.failures - baseReport.failures} |
-| flaky         | ${baseReport.flaky}    | ${diffReport.flaky}    | ${diffReport.flaky - baseReport.flaky}       |
-| duration (ms) | ${baseReport.duration} | ${diffReport.duration} | ${diffReport.duration - baseReport.duration} |
-`;
+const output = generateReport({
+  baseReport,
+  diffReport,
+  baseIdentifier,
+  diffIdentifier,
+});
 
 fs.writeFileSync(outputPath, output);
